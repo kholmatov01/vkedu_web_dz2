3fe651349000ee1372be500154ccd7e477845bf0
tkholmatov <kholmatov@student.bmstu.ru>
2024-11-13 17:14:28 +0300

    database problem

--- askme_kholmatov/app/models.py
+++ askme_kholmatov/app/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Profile(models.Model):
+    username = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+    avatar = models.ImageField
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=30)
+
+class Question(models.Model):
+    user = models.ForeignKey(settings.USER_AUTH_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    STATUS_CHOICES = [
+        ('s', 'Solved')
+        ('ns', 'Not Solved')
+    ]
+    status = models.CharField()
+    tag = models.ManyToManyField(Tag)
+
+
+class Answer(models.Model):
+    user = models.ForeignKey(settings.USER_AUTH_MODEL, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    body = models.TextField()
+    
+
+class QuestionLike(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    class Meta:
+        unique_together = ("question", "user")
+
+class AnswerLike(models.Model):
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    class Meta:
+        unique_together = ("answer", "user")
--- askme_kholmatov/askme_kholmatov/settings.py
+++ askme_kholmatov/askme_kholmatov/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,9 +77,13 @@ WSGI_APPLICATION = 'askme_kholmatov.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "askme_kholmatov_db",
+        "USER": "postgres",
+        "PASSWORD": "tanki567",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
